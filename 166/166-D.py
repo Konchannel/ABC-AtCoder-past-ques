ac484af2e7fb4fb1695bb13ef195308abf8d3685
Konchannel <konchan2416@gmail.com>
2020-05-05 23:58:15 +0900

    add: improvement

--- 166/166-D.py
+++ 166/166-D.py
@@ -17,3 +17,33 @@ improvement:
 
 
 # === improvement ===
+import math
+X = int(input())
+a1 = math.ceil(X**(1/5))
+N = int(1e10)
+c = 0
+b = 0
+b2 = 0
+while c <= N:
+    c += 1
+    if b**5 < -X:
+        b2 = b + 1
+        break
+    b -= 1
+
+c = 0
+while c <= N:
+    c += 1
+    b1 = math.floor((a1**5-X)**(1/5))
+    a2 = math.floor((X+b2**5)**(1/5))
+
+    print('a1:' + str(a1) + ' b1:' + str(b1) + ' b2:' + str(b2) + ' a2:' + str(a2) + ' c:' + str(c))
+
+    if a1**5 - b1**5 == X:
+        print(a1, b1)
+        break
+    elif a2**5 - b2**5 == X:
+        print(a2, b2)
+        break
+    a1 += 1
+    b2 += 1
