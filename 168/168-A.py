94ea9cf3269a3b298223ff7494d5a3be4ce28647
Konchannel <konchan2416@gmail.com>
2020-05-28 12:00:00 +0900

    add: tried for A

---
+++ 168/168-A.py
@@ -0,0 +1,18 @@
+"""
+
+===================
+tried-01:
+
+improvement:
+"""
+
+# === tried-01 ===
+flag = int(input()[-1])
+if flag in [2, 4, 5, 7, 9]:
+    print("hon")
+elif flag in [0, 1, 6, 8]:
+    print("pon")
+else:
+    print("bon")
+
+# === improvement ===
