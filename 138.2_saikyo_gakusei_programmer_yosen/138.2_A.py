f70f588838961ef16ed6226f84d80047df635783
Konchannel <konchan2416@gmail.com>
2020-06-01 12:00:00 +0900

    add: tried for A

---
+++ 138.2_saikyo_gakusei_programmer_yosen/138.2_A.py
@@ -0,0 +1,21 @@
+"""
+
+===================
+tried-01:
+
+improvement:
+"""
+
+# === tried-01 ===
+m, d = map(int, input().split())
+count = 0
+
+for i in range(2, m+1):
+    for j in range(20, d+1):
+        dt, do = divmod(j, 10)
+        if 1 < do and dt * do == i:
+            count += 1
+
+print(count)
+
+# === improvement ===
